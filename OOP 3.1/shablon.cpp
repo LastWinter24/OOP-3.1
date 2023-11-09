9346a29fff03f9e7c0d51c5a3067c74deb6f2a0c
LastWinter <neretin-205@bk.ru>
2023-11-09 10:23:47 +0300

    OOP 3.1

---
+++ OOP 3.1/shablon.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	class Company {
+	public:
+		string name; // название компании
+		int persons; // количество сотрудников
+		int money; // месячный фонд зарплаты
+	};
+	
+	return 0;
+}
+
+
+
